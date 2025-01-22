68bd9eb5dfa58d9cf81c13d156cebc57a892d717
Caleb Sellinger <calebsell24@gmail.com>
2025-01-21 18:39:43 -0600

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+######################################
+# Caleb Sellinger
+# Dr. Case
+# 44608, Data Analytics Fundamentals
+# 1-21-2025
+######################################
+
+name = "Bingus"
+
+print(f"Hello World! My name is {name}")
